e8bb21005766b16146f694c630497285892972e4
LBV 2012-26 <lbv2012-26@outlook.com>
2024-08-14 20:13:56 +0800

    Update

---
+++ NpgsCore/Sources/Engine/Core/Random.hpp
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include <random>
+#include "Engine/Core/Base.h"
+
+_NPGS_BEGIN
+
+class NPGS_API Distribution {
+public:
+    virtual ~Distribution() = default;
+    virtual double Generate(std::mt19937& Engine) = 0;
+};
+
+class NPGS_API UniformDistribution : public Distribution {
+public:
+    UniformDistribution(double Min, double Max) : _Distribution(Min, Max) {}
+
+    double Generate(std::mt19937& Engine) override {
+        return _Distribution(Engine);
+    }
+
+private:
+    std::uniform_real_distribution<double> _Distribution;
+};
+
+class NPGS_API NormalDistribution : public Distribution {
+public:
+    NormalDistribution(double Mean, double Sigma) : _Distribution(Mean, Sigma) {}
+
+    double Generate(std::mt19937& Engine) override {
+        return _Distribution(Engine);
+    }
+
+private:
+    std::normal_distribution<double> _Distribution;
+};
+
+class NPGS_API LogNormalDistribution : public Distribution {
+public:
+    LogNormalDistribution(double Mean, double Sigma) : _Distribution(Mean, Sigma) {}
+
+    double Generate(std::mt19937& Engine) override {
+        return _Distribution(Engine);
+    }
+
+private:
+    std::lognormal_distribution<double> _Distribution;
+};
+
+class NPGS_API BernoulliDistribution : public Distribution {
+public:
+    BernoulliDistribution(double Probability) : _Distribution(Probability) {}
+
+    double Generate(std::mt19937& Engine) override {
+        return _Distribution(Engine);
+    }
+
+private:
+    std::bernoulli_distribution _Distribution;
+};
+
+_NPGS_END
--- NpgsTest/Sources/stdafx.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#pragma warning(disable : 4996)
-
-#include <xstdafx.h>
-
-#include <fast-cpp-csv-parser/csv.h>
-#include <glm/glm.hpp>
-#include <spdlog/spdlog.h>
