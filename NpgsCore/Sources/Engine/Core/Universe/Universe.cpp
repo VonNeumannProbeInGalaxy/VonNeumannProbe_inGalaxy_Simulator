86db15e76b95b7b05adee3d566c40864dd304a4e
LBV 2012-26 <lbv2012-26@outlook.com>
2024-10-21 21:17:08 +0800

    Update

--- NpgsCore/Sources/Engine/Core/Universe/Universe.cpp
+++ NpgsCore/Sources/Engine/Core/Universe/Universe.cpp
@@ -4,7 +4,6 @@ module;
 #include <algorithm>
 #include <array>
 #include <format>
-#include <fstream>
 #include <future>
 #include <iomanip>
 #include <iterator>
@@ -196,65 +195,65 @@ void Universe::FillUniverse() {
 
     NpgsCoreInfo("Generating binary stars...");
     GenerateBinaryStars(MaxThread);
-    //NpgsCoreInfo("Sorting...");
-    //std::sort(Slots.begin(), Slots.end(), [](const glm::vec3& Point1, const glm::vec3& Point2) {
-    //    return glm::length(Point1) < glm::length(Point2);
-    //});
+    NpgsCoreInfo("Sorting...");
+    std::sort(Slots.begin(), Slots.end(), [](const glm::vec3& Point1, const glm::vec3& Point2) {
+        return glm::length(Point1) < glm::length(Point2);
+    });
 
-    //NpgsCoreInfo("Assigning name...");
-    //std::string Name;
-    //std::ostringstream Stream;
-    //for (auto& System : _StellarSystems) {
-    //    glm::vec3 Position = System.GetBaryPosition();
-    //    auto it = std::lower_bound(Slots.begin(), Slots.end(), Position, [](const glm::vec3& Point1, const glm::vec3& Point2) -> bool {
-    //        return glm::length(Point1) < glm::length(Point2);
-    //    });
-    //    std::ptrdiff_t Offset = it - Slots.begin();
-    //    Stream << std::setfill('0') << std::setw(8) << std::to_string(Offset);
-    //    Name = "SYSTEM-" + Stream.str();
-    //    System.SetBaryName(Name).SetBaryDistanceRank(Offset);
+    NpgsCoreInfo("Assigning name...");
+    std::string Name;
+    std::ostringstream Stream;
+    for (auto& System : _StellarSystems) {
+        glm::vec3 Position = System.GetBaryPosition();
+        auto it = std::lower_bound(Slots.begin(), Slots.end(), Position, [](const glm::vec3& Point1, const glm::vec3& Point2) -> bool {
+            return glm::length(Point1) < glm::length(Point2);
+        });
+        std::ptrdiff_t Offset = it - Slots.begin();
+        Stream << std::setfill('0') << std::setw(8) << std::to_string(Offset);
+        Name = "SYSTEM-" + Stream.str();
+        System.SetBaryName(Name).SetBaryDistanceRank(Offset);
 
-    //    auto& Stars = System.StarData();
-    //    if (Stars.size() > 1) {
-    //        std::sort(Stars.begin(), Stars.end(), [](const std::unique_ptr<Astro::Star>& Star1, std::unique_ptr<Astro::Star>& Star2) -> bool {
-    //            return Star1->GetMass() > Star2->GetMass();
-    //        });
+        auto& Stars = System.StarData();
+        if (Stars.size() > 1) {
+            std::sort(Stars.begin(), Stars.end(), [](const std::unique_ptr<Astro::Star>& Star1, std::unique_ptr<Astro::Star>& Star2) -> bool {
+                return Star1->GetMass() > Star2->GetMass();
+            });
 
-    //        char Rank = 'A';
-    //        for (auto& Star : Stars) {
-    //            Star->SetName("STAR-" + Stream.str() + " " + Rank);
-    //            ++Rank;
-    //        }
-    //    } else {
-    //        Stars.front()->SetName("STAR-" + Stream.str());
-    //    }
+            char Rank = 'A';
+            for (auto& Star : Stars) {
+                Star->SetName("STAR-" + Stream.str() + " " + Rank);
+                ++Rank;
+            }
+        } else {
+            Stars.front()->SetName("STAR-" + Stream.str());
+        }
 
-    //    Stream.str("");
-    //    Stream.clear();
-    //}
+        Stream.str("");
+        Stream.clear();
+    }
 
-    //NpgsCoreInfo("Reset home stellar system...");
-    //NodeType* HomeNode = _Octree->Find(glm::vec3(0.0f), [](const NodeType& Node) -> bool {
-    //    if (Node.IsLeafNode()) {
-    //        auto& Points = Node.GetPoints();
-    //        return std::find(Points.begin(), Points.end(), glm::vec3(0.0f)) != Points.end();
-    //    } else {
-    //        return false;
-    //    }
-    //});
+    NpgsCoreInfo("Reset home stellar system...");
+    NodeType* HomeNode = _Octree->Find(glm::vec3(0.0f), [](const NodeType& Node) -> bool {
+        if (Node.IsLeafNode()) {
+            auto& Points = Node.GetPoints();
+            return std::find(Points.begin(), Points.end(), glm::vec3(0.0f)) != Points.end();
+        } else {
+            return false;
+        }
+    });
 
-    //auto* HomeSystem = HomeNode->GetLink([](StellarSystem* System) -> bool {
-    //    return System->GetBaryPosition() == glm::vec3(0.0f);
-    //});
-    //HomeNode->RemoveStorage();
-    //HomeNode->AddPoint(glm::vec3(0.0f));
-    //HomeSystem->SetBaryNormal(glm::vec2(0.0f));
+    auto* HomeSystem = HomeNode->GetLink([](StellarSystem* System) -> bool {
+        return System->GetBaryPosition() == glm::vec3(0.0f);
+    });
+    HomeNode->RemoveStorage();
+    HomeNode->AddPoint(glm::vec3(0.0f));
+    HomeSystem->SetBaryNormal(glm::vec2(0.0f));
 
-    //for (auto& Star : HomeSystem->StarData()) {
-    //    Star->SetNormal(glm::vec3(0.0f));
-    //}
+    for (auto& Star : HomeSystem->StarData()) {
+        Star->SetNormal(glm::vec3(0.0f));
+    }
 
-    // NpgsCoreInfo("Star generation completed.");
+    NpgsCoreInfo("Star generation completed.");
 
     _ThreadPool->Terminate();
 }
@@ -496,46 +495,13 @@ void Universe::CountStars() {
     std::println("");
 
     for (auto& System : _StellarSystems) {
-        // bool bHasMassiveStar = false;
-        // bool bIsBinary = System.StarData().size() == 2;
-
-        // if (bIsBinary) {
-        //     ++TotalBinarys;
-        // } else {
-        //     ++TotalSingles;
-        // }
-
-        // if (bIsBinary) {
-        //     bHasMassiveStar =
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_O ||
-        //         System.StarData().back()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_O ||
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WN ||
-        //         System.StarData().back()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WN ||
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WC ||
-        //         System.StarData().back()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WC ||
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WO ||
-        //         System.StarData().back()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WO;
-        // } else {
-        //     bHasMassiveStar =
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_O ||
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WN ||
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WC ||
-        //         System.StarData().front()->GetStellarClass().Data().HSpectralClass == Modules::StellarClass::SpectralClass::kSpectral_WO;
-        // }
-
         for (auto& Star : System.StarData()) {
             ++TotalStars;
 
-            // if (bHasMassiveStar) {
-            //     std::println("{}", FormatInfo(Star.get()));
-            // }
-
-            if (Star->GetInitialMass() / kSolarMass >= 15 && Star->GetInitialMass() / kSolarMass < 20) {
-                if (Star->GetIsSingleStar()) {
-                    ++TotalSingles;
-                } else {
-                    ++TotalBinarys;
-                }
+            if (Star->GetIsSingleStar()) {
+                ++TotalSingles;
+            } else {
+                ++TotalBinarys;
             }
 
             const auto& Class = Star->GetStellarClass();
@@ -644,10 +610,6 @@ void Universe::CountStars() {
                 continue;
             }
         }
-
-        // if (bHasMassiveStar) {
-        //     std::println("");
-        // }
     }
 
     std::println("Most luminous main sequence star: luminosity: {}", MostLuminousMainSequence.LuminositySol);
@@ -775,23 +737,6 @@ void Universe::CountStars() {
     std::println("Number of single stars: {}", TotalSingles);
     std::println("Number of binary stars: {}", TotalBinarys);
     std::println("");
-
-    std::ofstream SingleStar("Tools/SingleStar.csv");
-    std::ofstream BinaryFirstStar("Tools/BinaryFirstStar.csv");
-    std::ofstream BinarySecondStar("Tools/BinarySecondStar.csv");
-
-    for (auto& System : _StellarSystems) {
-        if (System.StarData().size() > 1) {
-            BinaryFirstStar << System.StarData().front()->GetInitialMass() / kSolarMass << ",";
-            BinarySecondStar << System.StarData().back()->GetInitialMass() / kSolarMass << ",";
-        } else {
-            SingleStar << System.StarData().front()->GetInitialMass() / kSolarMass << ",";
-        }
-    }
-
-    SingleStar.close();
-    BinaryFirstStar.close();
-    BinarySecondStar.close();
 }
 
 void Universe::GenerateSlots(float MinDistance, std::size_t NumSamples, float Density) {
