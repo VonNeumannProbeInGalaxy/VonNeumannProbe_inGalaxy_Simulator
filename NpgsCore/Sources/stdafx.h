d2501df9d8f0a698bffacc299e4711f7c287fc90
LBV 2012-26 <lbv2012-26@outlook.com>
2024-12-31 17:17:51 +0800

    Update

--- NpgsCore/Sources/stdafx.h
+++ NpgsCore/Sources/stdafx.h
@@ -8,15 +8,8 @@
 #include <fast-cpp-csv-parser/csv.h>
 #include <glad/glad.h>
 
-#ifdef VULKAN
-#define GLFW_INCLUDE_VULKAN
-#define VK_USE_PLATFORM_WIN32_KHR
-#endif // VULKAN
 #include <GLFW/glfw3.h>
 
-#ifdef VULKAN
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#endif // VULKAN
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
